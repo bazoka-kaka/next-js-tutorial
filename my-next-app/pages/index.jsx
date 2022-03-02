b99b533167046f10f2bd255c1a650e23fab13073
Yehezkiel Wiradhika <tsukikagemaikaka1@gmail.com>
2022-03-02 22:04:22 +0700

    Add 05. Creating a layout component

--- my-next-app/pages/index.jsx
+++ my-next-app/pages/index.jsx
@@ -1,12 +1,7 @@
-import Navbar from "/components/Navbar.jsx";
-import Footer from "/components/Footer.jsx";
-
 export default function Home() {
   return (
     <div>
-      <Navbar />
       <h1>Home Page</h1>
-      <Footer />
     </div>
   );
 }
