60d2932d15727e8ece0353ed4db48dfc76139a7c
Yehezkiel Wiradhika <tsukikagemaikaka1@gmail.com>
2022-03-03 12:40:20 +0700

    Add 11. Dynamic routes part 1

--- my-next-app/pages/contacts/index.jsx
+++ my-next-app/pages/contacts/index.jsx
@@ -1,9 +1,10 @@
 import Head from "next/head";
 import styles from "../../styles/Contacts.module.css";
+import Link from "next/link";
 
 export const getStaticProps = async () => {
-  const res = await fetch("https://jsonplaceholder.typicode.com/users");
-  const data = await res.json();
+  const url = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await url.json();
 
   return {
     props: { contacts: data },
@@ -21,12 +22,12 @@ const Contacts = ({ contacts }) => {
         <h1>Contacts Page</h1>
         {contacts.map((contact) => {
           return (
-            <div key={contact.id}>
+            <Link href={`/contacts/${contact.id}`} key={contact.id}>
               <a className={styles.single}>
                 <h3>{contact.name}</h3>
                 <p>Email: {contact.email}</p>
               </a>
-            </div>
+            </Link>
           );
         })}
       </div>
